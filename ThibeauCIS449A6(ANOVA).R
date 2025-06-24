6efda04d8ada787c5c0dab5809bc59346827ca9f
Tyler Thibeau <thomas.thibeau@maine.edu>
2025-06-24 18:31:07 +0000

    43

--- ThibeauCIS449A6(ANOVA).R
+++ ThibeauCIS449A6(ANOVA).R
@@ -1,2 +1,11 @@
 paint <- read.csv(file.choose(), header=T)
 names(paint)
+
+plot(paint) ## see what's going on with all x/y options
+
+## see what we can do with red on red. Looks like this will correlate well
+plot(paint$RA, paint$R1)
+
+cor(paint$RA, paint$R1)
+
+attach(paint) ## make it easier to work with here
