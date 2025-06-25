2bdff9fbe108e95c8108a7ca3ab0981da4a8ed46
Tyler Thibeau <thomas.thibeau@maine.edu>
2025-06-25 13:16:00 +0000

    regression line won't render

--- ThibeauCIS449A6(ANOVA).R
+++ ThibeauCIS449A6(ANOVA).R
@@ -1,65 +1,15 @@
 library(MASS)
-## idk if I need the library line in here. I wish I had time to play around...
+## first part, use the RGB file
+rgb = read.csv(file.choose(), header=T)
 
-paint <- read.csv(file.choose(), header=T)
-names(paint)
+columns = names(rgb)
+columns
+## [1] "ColorID"   "ColorName" "RA"        "GA"        "BA"        "R1"        "G1"        "B1"
+## [9] "R2"        "G2"        "B2"
 
-plot(paint) ## see what's going on with all x/y options
-
-## why, because AI said so and I have like 45 minutes to do this
-if (!exists("paint") || !is.data.frame(paint)) {
-  stop("The 'data' object does not exist or is not a data frame.")
-}
-
-
-columns <- colnames(paint)
-
-## iterate over the columns
-for (col in columns)
-{
-  if (is.numeric(paint[[col]]))
-  {  # Check if the column is numeric
-    for (col2 in columns)
-      {
-      if (is.numeric(paint[[col2]]) && col != col2)
-        {  # Check if the second column is numeric and not the same as the first
-        correlation_value <- cor(paint[[col]], paint[[col2]], use = "complete.obs")  # Calculate correlation
-        print(paste("Correlation between", col, "and", col2, "is", correlation_value))
-
-        ## does this work up here?
-        k =  lm(paint[[col]]~paint[[col2]], data=paint)
-        k
-
-      }
-    }
-  }
-}
-
-## ooh, I think he wants this, not ^^ that... shit this is gunna take more time than I got..
-k =  lm(paint$RA~paint$R1, data=paint)
-k
-
-summary(paint)
-
-#####################################################################
-led <- read.csv(file.choose(), header=T)
-
-plot(led$R, led$R.LED.on.)
-plot(led$B, led$B.LED.on.)
-plot(led$G, led$G.LED.on.)
-
-plot(led$R, led$R.Red.)
-plot(led$B, led$B.Blue.)
-plot(led$G, led$G.Green.)
-
-
-cor(led$R, led$R.LED.on.)
-cor(led$B, led$B.LED.on.)
-cor(led$G, led$G.LED.on.)
-
-cor(led$R, led$R.Red.)
-cor(led$B, led$B.Blue.)
-cor(led$G,led$G.Green.)
-
-
-## I have no idea what this is asking me to do...
+## lm for the linear regression
+rr1 <- lm(rgb$RA~rgb$R1, data = rgb)
+summary(rr1)
+## see what it looks like
+plot(rgb$RA,rgb$R1)
+abline(rr1, col = "red")
