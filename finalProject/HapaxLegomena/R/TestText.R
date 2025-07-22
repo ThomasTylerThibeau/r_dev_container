22d6842d959d8360ac6608a3038dda744cd8ecce
Tyler Thibeau <thomas.thibeau@maine.edu>
2025-07-22 14:10:10 +0000

    errors

--- finalProject/HapaxLegomena/R/TestText.R
+++ finalProject/HapaxLegomena/R/TestText.R
@@ -8,5 +8,15 @@
 
 NULL
 
-text <- readLines("../textFiles/TestText.txt")
+## get the filepath from
+## HapaxLegomena/R/TestText.R -> HapaxLegomena/textFiles/TestText.txt
+path <- system.file("textFiles", "TestText.txt", package="HapaxLegomena")
 
+## read the file
+if(file.exists(path)){
+  redlines = readLines(path)
+} else {
+  stop("Shit... something fucked up.")
+} ## gawdy... I hate the unaligned brackets. ugly ugly ugly
+
+## now we create a wordcount for all the words
