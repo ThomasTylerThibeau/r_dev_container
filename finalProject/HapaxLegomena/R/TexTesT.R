0d4d30755e08ba0f763905ee894813218458a2b9
Tyler Thibeau <thomas.thibeau@maine.edu>
2025-07-22 19:11:50 +0000

    TexTesT

---
+++ finalProject/HapaxLegomena/R/TexTesT.R
@@ -0,0 +1,7 @@
+#' This is pretty much just a text file with a back story for the curious.
+#' It exists entirely to test the functions out and see how they behave.
+#'
+#' @format text
+#'
+#' @example data(TexTesT)
+"TexTesT"
