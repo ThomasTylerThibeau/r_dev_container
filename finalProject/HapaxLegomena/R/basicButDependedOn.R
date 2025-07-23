051c53651ccba5d73c20d01c4abb928d55fcf90f
Tyler Thibeau <thomas.thibeau@maine.edu>
2025-07-23 13:00:20 +0000

    still figuring it out

--- finalProject/HapaxLegomena/R/basicButDependedOn.R
+++ finalProject/HapaxLegomena/R/basicButDependedOn.R
@@ -3,3 +3,8 @@ minmax <- function(x, ...){
 }
 ## basic function that is seen as a dependency of others
 ## ... allows the user to add their own arguments
+
+
+## this actually has nothing to do with the package, but I learned about
+## testing functions that are depended on, and felt I should put it to use
+## and perhaps down the line do this for something else in the package, maybe
